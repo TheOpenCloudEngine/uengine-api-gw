1545591d772c0a5cded4f159547ffc2aef809810
SeungpilPark <sppark@uengine.org>
2016-05-26 19:04:12 +0900

    project start

---
+++ iam-proxy/src/main/java/org/opencloudengine/garuda/TransportProtocol.java
@@ -0,0 +1,8 @@
+package org.opencloudengine.garuda;
+
+/**
+ * Enumeration of transport protocols supported by LittleProxy.
+ */
+public enum TransportProtocol {
+    TCP, UDT
+}
---
+++ iam-wrapper/src/main/java/Test.java
@@ -0,0 +1,5 @@
+/**
+ * Created by uengine on 2016. 5. 26..
+ */
+public class Test {
+}
